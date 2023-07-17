37232e3ac5532de2afd59e96cd49f1aabf5d139c
Brahimovith <brahim.sabri.contact@gmail.com>
2023-07-17 16:56:34 -0700

    hhhh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+/**
+ * main -  fonction puts
+ * Description: 'prog printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
 
-int main()
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
-}
 
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+
+}
