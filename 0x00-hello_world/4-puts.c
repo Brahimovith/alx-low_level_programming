0f5c3439ae42e2251cced9d91ece148fe33d3fcb
Brahimovith <brahim.sabri.contact@gmail.com>
2023-07-17 16:12:23 -0700

    jjj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main -  fonction puts
+ * return: success
+ */
 int main(void)
 
 {
 
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
