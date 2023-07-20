c08ba099ba07f4565a0a2481eaa33b6de8a00f0d
Brahimovith <brahim.sabri.contact@gmail.com>
2023-07-20 05:16:41 -0700

    lll

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	for(int i=48 ,i<=57, i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
