0d276e454afe6041ff35831aebf5076122a05a67
Brahimovith <brahim.sabri.contact@gmail.com>
2023-07-20 07:40:21 -0700

    jdj

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * mul - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplicattion
  */
+
 int mul(int a, int b)
 {
-	return(a*b);
+	return (a * b);
 }
