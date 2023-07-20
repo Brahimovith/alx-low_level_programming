1bb18f100e2f2d6ab7ff5904a55b9d737736b02e
Brahimovith <brahim.sabri.contact@gmail.com>
2023-07-20 07:55:08 -0700

    jjkl

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_numbers - print 0-9
+ * return: void
  */
+
 void print_numbers(void)
 {
-	for(int i=48 ,i<=57, i++)
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
