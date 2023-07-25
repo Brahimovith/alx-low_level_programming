f839d4f9b8689c37becf41418b5264b3ee948c3e
Brahimovith <brahim.sabri.contact@gmail.com>
2023-07-25 05:43:20 -0700

    hjh

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * rest_to_98 - check the code 
- *@*n: pointeur
+ * description: fct
+ *rest_to_98 - check the code
+ *@n: pointeur
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
